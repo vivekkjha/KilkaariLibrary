33fe7d5390d542db1d6df6bd7feddd43f528bf3b
vivek <vivekkjha@live.com>
2015-03-16 19:06:20 +0530

    commit from office

--- app/src/main/java/org/kilkaari/library/activities/MainActivity.java
+++ app/src/main/java/org/kilkaari/library/activities/MainActivity.java
@@ -1,9 +1,7 @@
 package org.kilkaari.library.activities;
 
-import android.support.v7.app.ActionBarActivity;
+
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import org.kilkaari.library.R;
 
