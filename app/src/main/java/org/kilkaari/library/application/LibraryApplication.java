8e9dd2e391750b9c58e065e37b74ff6fb17a6284
vivekjha <vivekkjha@live.com>
2015-03-17 23:11:35 +0530

    Parse library and application file

--- app/src/main/java/org/kilkaari/library/application/LibraryApplication.java
+++ app/src/main/java/org/kilkaari/library/application/LibraryApplication.java
@@ -2,6 +2,8 @@ package org.kilkaari.library.application;
 
 import android.app.Application;
 
+import com.parse.Parse;
+
 /**
  * Created by vk on 14-03-2015.
  */
@@ -10,5 +12,12 @@ public class LibraryApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Enable Local Datastore.
+
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "wF57BFoc8Sge0vz1tWb6vrHEOSr6BffUWULKXjpF", "KojP7xdhyKwULJNT0TglZbKyfwDoBNDouqkVze1v");
+
+
     }
 }
